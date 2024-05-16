f50f459a35e69959c1c440fcb76f4578e4943afb
Des <wcabrera-2022060@kinal.edu.gt>
2024-05-16 14:04:22 -0600

    Updated moongose
    Se elimino código innecesario del back
    
    Co-Authored-By: lcruz-2019072 <135739841+Lcruz-2019072@users.noreply.github.com>
    Co-Authored-By: apacheco-2022046 <135729728+apacheco-2022046@users.noreply.github.com>
    Co-Authored-By: Hermes Batres  <135688545+hbatres-2022143@users.noreply.github.com>
    Co-Authored-By: jsipac-2022133 <135518156+jsipac-2022133@users.noreply.github.com>

--- storage/BackEnd/src/config/mongoose.js
+++ storage/BackEnd/src/config/mongoose.js
@@ -8,21 +8,6 @@ export const connect = async ()=>{
             console.log('MongoDB | could not be connect to mongodb')
             mongoose.disconnect()
         })
-        mongoose.connection.on('connecting', ()=>{
-            console.log('MongoDB | try connecting')
-        })
-        mongoose.connection.on('connected', ()=>{
-            console.log('MongoDB | connected to mongodb')
-        })
-        mongoose.connection.once('open', async ()=>{
-            console.log('MongoDB | connected to database');
-        })
-        mongoose.connection.on('reconnected', ()=>{
-            console.log('MongoDB | reconected to mongodb')
-        })
-        mongoose.connection.on('disconnected', ()=>{
-            console.log('MongoDB | disconnected')
-        })
         await mongoose.connect(process.env.URI_MONGO, {
             serverSelectionTimeoutMS: 5000,
             maxPoolSize: 50
